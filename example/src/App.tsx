6165c4d632ea4d89df2a7dc6e64d11efa4362022
Eslam ElMeniawy <eslam.elmeniawy@gmail.com>
2023-09-09 15:22:52 +0300

    docs: change package import in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-common-components';
+import { multiply } from '@eslam-elmeniawy/react-native-common-components';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
