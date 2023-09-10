a04b7d634f7f1350b875a7580722f910899a8738
Eslam ElMeniawy <eslam.elmeniawy@gmail.com>
2023-09-10 22:14:18 +0300

    feat: add `Card` component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,16 @@
+// External imports.
+import React from 'react';
+import { withTheme, Card as PaperCard } from 'react-native-paper';
+
+// Types imports.
+import type { PropsWithTheme } from './Card.types';
+
+// Internal imports.
+import styles from './Card.styles';
+
+const Card = (props: PropsWithTheme): React.ReactElement => {
+  const { style, ...other } = props;
+  return <PaperCard style={[styles.card, style]} {...other} />;
+};
+
+export default withTheme(Card);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './AlertDialog';
 export * from './Button';
+export * from './Card';
 export * from './Dialog';
 export * from './IconButton';
 export * from './Text';
