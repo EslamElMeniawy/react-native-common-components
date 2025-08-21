da3c123c263eaca9109e3b7597206e35f4bd312a
Eslam El-Meniawy <eelmeniawy@ejada.com>
2025-08-21 09:29:45 +0300

    chore: add `babel-plugin-module-resolver`

--- babel.config.js
+++ babel.config.js
@@ -9,4 +9,16 @@ module.exports = {
       presets: ['module:@react-native/babel-preset'],
     },
   ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        alias: {
+          '@src': './src',
+        },
+      },
+    ],
+  ],
 };
