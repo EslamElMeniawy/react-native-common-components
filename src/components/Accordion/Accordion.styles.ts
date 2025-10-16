254f0719078c72a8d29ced8001a93b6281f6d751
Eslam El-Meniawy <eelmeniawy@ejada.com>
2025-10-16 16:55:27 +0300

    feat: add `Accordion` component

---
+++ src/components/Accordion/Accordion.styles.ts
@@ -0,0 +1,18 @@
+// External imports.
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  headerRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerContainer: {
+    flex: 1,
+  },
+  measuringContainer: {
+    position: 'absolute',
+    opacity: 0,
+  },
+});
+
+export default styles;
